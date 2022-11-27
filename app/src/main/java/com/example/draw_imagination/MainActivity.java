b30c0abc0a70429c63c52538bf676ffa9614efee
lcj1204 <dlckdwo1204@naver.com>
2022-11-27 18:39:30 +0900

    Feat: 3 fragment test

--- app/src/main/java/com/example/draw_imagination/MainActivity.java
+++ app/src/main/java/com/example/draw_imagination/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.draw_imagination;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.TabActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class MainActivity extends AppCompatActivity {
+@SuppressWarnings("deprecation")
+//public class MainActivity extends AppCompatActivity {
+public class MainActivity extends TabActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = getTabHost();
+
+        TabHost.TabSpec tabSpec1 = tabHost.newTabSpec("GENERATION").setIndicator("생성");
+        tabSpec1.setContent(R.id.tab1);
+        tabHost.addTab(tabSpec1);
+
+        TabHost.TabSpec tabSpec2 = tabHost.newTabSpec("VARIATION").setIndicator("변환");
+        tabSpec2.setContent(R.id.tab2);
+        tabHost.addTab(tabSpec2);
+
+        TabHost.TabSpec tabSpec3 = tabHost.newTabSpec("GALLERY").setIndicator("갤러리");
+        tabSpec3.setContent(R.id.tab3);
+        tabHost.addTab(tabSpec3);
+
+        tabHost.setCurrentTab(0);
     }
 }
