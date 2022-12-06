435d9ae7013e000f0c32a6a4191b17eccdf0ad6b
lcj1204 <dlckdwo1204@naver.com>
2022-12-07 00:17:34 +0900

    FEAT : Glide를 쓰기위한 class

---
+++ app/src/main/java/com/example/draw_imagination/GlideModule.java
@@ -0,0 +1,7 @@
+package com.example.draw_imagination;
+
+import com.bumptech.glide.module.AppGlideModule;
+
+@com.bumptech.glide.annotation.GlideModule
+public class GlideModule extends AppGlideModule {
+}
