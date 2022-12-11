aa08f97d007deaf397eca5b65333056aa366ca9a
lcj1204 <dlckdwo1204@naver.com>
2022-12-11 21:14:54 +0900

    FEAT: 이미지 추가

--- app/src/main/java/com/example/draw_imagination/Fragment_gallery.java
+++ app/src/main/java/com/example/draw_imagination/Fragment_gallery.java
@@ -27,6 +27,12 @@ public class Fragment_gallery extends Fragment {
             R.drawable.img3_1,
             R.drawable.img3_2,
             R.drawable.img3_3,
+            R.drawable.img4_1,
+            R.drawable.img4_2,
+            R.drawable.img4_3,
+            R.drawable.img5_1,
+            R.drawable.img5_2,
+            R.drawable.img5_3,
     };
 
     private String[] texts = new String[] {
@@ -39,6 +45,12 @@ public class Fragment_gallery extends Fragment {
             "a photo of alamo square, san francisco, from a street at night",
             "a photo of the food of china",
             "a snail made of harp",
+            "파란 모자, 빨간 장갑, 녹색 셔츠, 그리고 노란 바지를 입은 아기 펭귄의 이모지",
+            "테이블 위에 놓여있는 유리잔",
+            "오각형의 녹색 시계",
+            "거대한 산호의 사진",
+            "호두의 단면도",
+            "호메로스의 흉상 사진",
     };
 
     private String[] styles = new String[] {
@@ -48,6 +60,12 @@ public class Fragment_gallery extends Fragment {
             "painting",
             "pop art",
             "pencil sketch",
+            " X ",
+            " X ",
+            " X ",
+            " X ",
+            " X ",
+            " X ",
             " X ",
             " X ",
             " X ",
